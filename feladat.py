81d2edeb65c04a5136d8e377b92abad72c0aadcf
RLevente01 <144986695+RLevente01@users.noreply.github.com>
2023-09-21 08:19:13 +0200

    Add files via upload

---
+++ feladat.py
@@ -0,0 +1,61 @@
+#1. feladat: kérjünk be 2 darab egész számot és vizsgáljuk meg. Irja ki hogy melyik a nagyobb érték vagy egyenlőek e
+"""
+szam=int(input("Kérek egy számot: "))
+szam2=int(input("Kérek egy számot: "))
+if szam == szam2:
+    print("A két szám megegyező")
+elif szam > szam2:
+    print( szam, "nagyobb mint", szam2)
+    
+elif szam < szam2:
+    print(szam2 ,"nagyobb mint", szam)
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+#kérjünk be 5 db egész számot meg kell vizsgálni hogy pozitiv vagy negativ illetve nagyobb e mint 20
+#megszámolni hogy hány db ami pozitiv
+
+sz1=int(input("Kérek egy számot: "))
+sz2=int(input("Kérek egy számot: "))
+sz3=int(input("Kérek egy számot: "))
+sz4=int(input("Kérek egy számot: "))
+sz5=int(input("Kérek egy számot: "))
+
+if sz1> 0:
+    print("pozitiv")
+else:
+     print("negativ")
+
+if sz2> 0:
+    print("pozitiv")
+else:
+     print("negativ")
+     
+if sz3> 0:
+    print("pozitiv")
+else:
+     print("negativ")
+     
+if sz4> 0:
+    print("pozitiv")
+else:
+     print("negativ")
+
+if sz5> 0:
+    print("pozitiv")
+else:
+     print("negativ")
+ 
+
+
+
