40b87b39a09e49aae20af853f3224d810cd3433c
RLevente01 <144986695+RLevente01@users.noreply.github.com>
2023-09-29 09:09:35 +0200

    Add files via upload

---
+++ ciklus.py
@@ -0,0 +1,14 @@
+"""
+for x in range(1,11,1):
+    print(f" {x}. kiírás Levente")
+
+for x in range(1,101,2):
+    print(x)
+
+for x in range(1,6,1):
+    szam = int(input("Kérem a számot:"))
+    novelt_ertek = szam + 2
+    print(f"A megnövelt érték: {novelt_ertek}")
+""" 
+for x in range(50,0,-1):
+    print(x)
